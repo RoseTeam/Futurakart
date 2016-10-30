64d4c9385deb63400073bf17973fd3a612be5ced
vfdev-5 <vfdev-5@gmail.com>
2016-10-30 18:08:06 +0100

    * Add verbose option to bringup vision

--- src/futurakart_robot/futurakart_bringup/scripts/start_vision.py
+++ src/futurakart_robot/futurakart_bringup/scripts/start_vision.py
@@ -15,6 +15,7 @@ def parse_args(argv):
     username = None
     port = 22
     path = None
+    verbose = False
     for arg in argv:
         splt = arg.split(":=")
         if splt[0] == "ip":
@@ -25,9 +26,11 @@ def parse_args(argv):
             port = splt[1]
         elif splt[0] == "path":
             path = splt[1]
+        elif splt[0] == "verbose":
+            verbose = True if splt[1] == "true" else False
 
     assert ip is not None and username is not None and path is not None, "Failed to parse given arguments : %s " % argv
-    return ip, username, port, path
+    return ip, username, port, path, verbose
 
 
 def handle_signint(*args, **kwargs):
@@ -42,13 +45,13 @@ if __name__ == "__main__":
     assert len(sys.argv) > 1, "Usage: script.py ip:=1.2.3.4 username:=temp path:=~/futurakart_ws [port:=22]"
 
     print "Start 'start_vision' node : communicate on ssh *vision* part RPi"
-    ip, username, port, path = parse_args(sys.argv[1:])
+    ip, username, port, path, verbose = parse_args(sys.argv[1:])
 
     # Need to catch sigint otherwise node is not properly killed
     signal.signal(signal.SIGINT, handle_signint)
 
     program = ["ssh", "-T", "-o", "VerifyHostKeyDNS no", "%s@%s" % (username, ip), "-p %s" % port]
-    proc = subprocess.Popen(program, stdin=subprocess.PIPE)
+    proc = subprocess.Popen(program, stdin=None if not verbose else subprocess.PIPE)
     # proc.communicate("cd %s; ls; ping www.google.com" % path)
     proc.communicate("cd %s; source devel/setup.bash; roslaunch futurakart_base vision.launch" % path)
     returncode = proc.wait()
